c1ec1920825de8eec3592e9f9fa55de7801adae8
DuvanCeballos <elber.ceballos@sophossolutions.com>
2021-06-18 12:05:58 -0500

    version 2

--- prueba1/src/test/java/com/prueba1/patterns/runners/Login.java
+++ prueba1/src/test/java/com/prueba1/patterns/runners/Login.java
@@ -1,5 +1,10 @@
 package com.prueba1.patterns.runners;
 
+import cucumber.api.*;
+import org.junit.runner.*;
+
+@RunWith(cucumber.class)
+
 public class Login {
 
 }
--- prueba1/src/test/java/com/prueba1/patterns/stepdefinitions/LoginStepDefinitions.java
+++ prueba1/src/test/java/com/prueba1/patterns/stepdefinitions/LoginStepDefinitions.java
@@ -1,5 +1,18 @@
 package com.prueba1.patterns.stepdefinitions;
 
 public class LoginStepDefinitions {
-
+	LinioHomePage LinioHome;
+	LinioLogin LinioLogin;
+	
+	@Given("")
+	public void navegarAPagina(String url) {
+		LinioHome.navegateTo(url);
+	}
+	
+	@When("")
+	public void IngresarLogin(String user, String password) {
+		LinioLogin.login(user,password);
+	}
+	
+	
 }
